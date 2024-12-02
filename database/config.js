5341f1fbce7c8054e6b2709db0bb7e6256c201cc
Javier <javiersorucoaltamirano19@gmail.com>
2024-12-02 10:18:40 -0400

    soloUri

--- database/config.js
+++ database/config.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CNN, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_CNN);
 
     console.log("DB Online");
   } catch (error) {
     console.error("Error de conexión a la base de datos:", error.message);
-    throw new Error("Error a la hora de inicializar DB", error.message);
+    throw new Error(`Error a la hora de inicializar DB: ${error.message}`);
   }
 };
 
